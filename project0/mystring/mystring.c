b43d84f516386f432f365367dc42a710b55bb746
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2019-09-04 14:06:20 -0600

    mystrncpy() implemented

--- project0/mystring/mystring.c
+++ project0/mystring/mystring.c
@@ -3,11 +3,30 @@
 
 char *mystrncpy(char *dest, const char *src, size_t n)
 {
-	return NULL;
+	size_t i;
+
+	// Allocate enough size in dest to copy src into it
+	dest = malloc(sizeof(char) * n); 
+	
+	// Check to see if src is shorter than the number of bytes
+	if(strlen(src) < n) {
+		strcpy(dest, src);
+		for (i = strlen(src); i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+		
+	} else {
+		for (i = 0; i < n; i++) {
+			dest[i] = src[i];
+		}
+	}
+	
+	return dest;
 }
 
 char *mystrncat(char *dest, const char *src, size_t n)
-{
+{	
 	return NULL;
 }
 
--- project0/mystring/mytests.c
+++ project0/mystring/mytests.c
@@ -1,7 +1,16 @@
 #include "mystring.h"
 
-
 int main(int argc, char**argv)
 {
-	printf("fix me\n");
+	//printf("fix me\n");
+
+	/* Test out the mystrncpy function */
+	char* dest;
+	char* src = "He";
+
+	//dest = mystrncpy(dest, src, 4);
+
+	printf("The mystrncpy string is: %s\n", mystrncpy(dest, src, 4));
+
+	return 0;
 }
