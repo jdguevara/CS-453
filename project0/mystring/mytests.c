4be8d53389506953c520ea1be9cb88a83961f541
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-09-04 15:44:42 -0600

    mytests output messages updated for mystrncpy()

--- project0/mystring/mytests.c
+++ project0/mystring/mytests.c
@@ -5,12 +5,23 @@ int main(int argc, char**argv)
 	//printf("fix me\n");
 
 	/* Test out the mystrncpy function */
-	char* dest1;
-	char* src = "Hello World!";
+	char* dest1; 
+	char* src1 = "Hello World!";
 
-	dest1 = mystrncpy(dest1, src, 4);
+	printf("String to copy using mystrncpy(): '%s'\n\n", src1);
+	// Test Case #1: n-bytes is less than src length
+	dest1 = mystrncpy(dest1, src1, 4);
 
-	printf("The mystrncpy string is: %s\n", dest1);
+	printf("Case 1: Bytes copied are less than string length\n");
+	printf("\tBytes copied: 4\n");
+	printf("\tResulting copy: '%s'\n\n", dest1);
+
+	// Test Case #2: n-bytes is longer than src length
+	dest1 = mystrncpy(dest1, src1, 16);
+	
+	printf("Case 2: Bytes copied are more than string length\n");
+	printf("\tBytes copied: 16\n");
+	printf("\tResulting copy: '%s'", dest1);
 
 	// Free memory
 	free(dest1);
