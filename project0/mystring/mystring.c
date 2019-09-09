2816f92a955fcb8d12338b92e8fb11667aa9bcb4
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-09-08 20:11:07 -0600

    Updates to ignore pile, and to mystring project

--- project0/mystring/mystring.c
+++ project0/mystring/mystring.c
@@ -1,4 +1,6 @@
 #include "mystring.h"
+#include <stdlib.h>
+#include <string.h>
 
 /*
 	Implementation of mystrncpy(), where the functions 
@@ -34,16 +36,88 @@ char *mystrncpy(char *dest, const char *src, size_t n)
 	return dest;
 }
 
+/*
+	Implementation of mystrncat(), where the function
+	should concatenate n-bytes of src to dest, and should
+	always be null-terminated. If the src string length is
+	greater than n-bytes then dest should only have n-bytes
+	+ a null character added to it. Otherwise, all of src
+	and the null character.
+
+	params: char * dest - Our destination pointer
+		    char * src - Our source string/pointer
+			size_t n - The determined number of bytes to concat
+						to dest from src
+	
+	return: properly allocated string with the correct number of
+		    bytes concatenated from src.
+ */
 char *mystrncat(char *dest, const char *src, size_t n)
-{	
-	return NULL;
+{
+    char* newDest; // Need somewhere to hold dest byte-for-byte
+    size_t dest_len = strlen(dest);
+    size_t i;
+
+    newDest = malloc(dest_len + (sizeof(char)*n) + 1);
+
+    // Copy over the stuff in dest (without this, the string in dest is overridden)
+    for (i = 0; i < dest_len; i++) {
+        newDest[i] = dest[i];
+    }
+
+    // Add n-bytes from source until the null character is found
+    for (i = 0 ; i < n && src[i] != '\0' ; i++)
+        newDest[dest_len + i] = src[i];
+
+    // Cap the string with null character
+    newDest[dest_len + i] = '\0';
+
+    return newDest;
 }
 
+/*
+ * Implementation of the mystrncmp() function in mystring.h
+ * It should take in two strings, compare the first n characters
+ * of each string and determine whether the first string is
+ * greater than, less than, or equal to the second string.
+ *
+ * Params: const char * s1
+ *         const char * s2
+ *
+ * Return: -1 if s1 < s2
+ *          0 if s1 = s2
+ *          1 if s1 > s2
+ */
 int mystrncmp(const char *s1, const char *s2, size_t n)
 {
-	return -1;
+    size_t i;
+    int val;
+
+    val = 0;
+    for (i = 0; i < n || s1[i] != '\0' || s2[i] != '\0'; ++i) {
+        val += s1[i] - s2[i];
+    }
+
+    if (val == 0) {
+        return 0;
+    } else if (val > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
 }
 
+/*
+ * Implementation of the tokenizer( ) function from mystring.h
+ * Parse a given string into tokens using provide delimiters. This function
+ * returns a token_data struct that contains the array of tokens and the count
+ * of tokens found.
+ *
+ * Params: const char * line - line/string to tokenize
+ *         char * delimiter - series of characters that determines how string is split
+ *
+ * Return:
+ */
 struct token_data *tokenizer(const char *line, char *delimiter)
 {
 	return NULL;
--- project0/mystring/mytests.c
+++ project0/mystring/mytests.c
@@ -1,13 +1,18 @@
 #include "mystring.h"
+#include <stdlib.h>
 
+/*
+ * Tests written for my implementations of the string modifier functions
+ *
+ * @author: Jaime Guevara
+ */
 int main(int argc, char**argv)
 {
-	//printf("fix me\n");
-
 	/* Test out the mystrncpy function */
 	char* dest1; 
 	char* src1 = "Hello World!";
 
+	printf("** Begin - Copy strings **\n\n");
 	printf("String to copy using mystrncpy(): '%s'\n\n", src1);
 	// Test Case #1: n-bytes is less than src length
 	dest1 = mystrncpy(dest1, src1, 4);
@@ -16,6 +21,9 @@ int main(int argc, char**argv)
 	printf("\tBytes copied: 4\n");
 	printf("\tResulting copy: '%s'\n\n", dest1);
 
+	// Free memory after use
+	free(dest1);
+
 	// Test Case #2: n-bytes is longer than src length
 	dest1 = mystrncpy(dest1, src1, 16);
 	
@@ -23,7 +31,77 @@ int main(int argc, char**argv)
 	printf("\tBytes copied: 16\n");
 	printf("\tResulting copy: '%s'", dest1);
 
+    // Free memory
+    free(dest1);
+
+	/* Test out mystrncat function */
+	char* dest2 = "Hello There! ";
+	char* src2  = "General Kenobi";
+	size_t n2 = 7;
+
+	printf("\n\n** Strings will be concatenated now! ** \n");
+	printf("\nDest string for mystrncat(): '%s'\n", dest2);
+	printf("Src string for mystrncat(): '%s'\n\n", src2);
+
+	// Test Case #1: n-bytes is less than src length
+	dest2 = mystrncat(dest2, src2, n2);
+
+	printf("Case 1: Bytes copied are less than src string length\n");
+	printf("\tBytes copied: %ld\n", n2);
+	printf("\tResulting concatenation: '%s'\n\n", dest2);
+
 	// Free memory
-	free(dest1);
-	return 0;
+	free(dest2);
+
+    // Test Case #2: n-bytes greater than both strings
+    n2 = 20;
+    dest2 = "Hello There! ";
+    dest2 = mystrncat(dest2, src2, n2);
+
+    printf("Case 2: Bytes copied are greater than both string lengths\n");
+    printf("\tBytes copied: %ld\n", n2);
+    printf("\tResulting concatenation: '%s'\n\n", dest2);
+
+    // Free memory
+    free(dest2);
+
+    // Test Case #3: empty dest
+
+    /* Testing out mystrncmp() */
+    printf("** Strings will be compared now! **\n"
+           "Number of bytes used throughout: 5\n\n");
+    // Test Case #1: "Same" string but with one character caps locked
+	char* str1 = "hello";
+    char* str2 = "Hello";
+
+    printf("Case #1: Same word, one character is caps locked\n");
+    printf("\tString 1: '%s'\n", str1);
+    printf("\tString 2: '%s'\n", str2);
+    printf("\tResult: %d\n\n", mystrncmp(str1, str2, 5));
+
+    // Test Case #2: Same string
+    str1 = "Hello";
+
+    printf("Case #2: Same word\n");
+    printf("\tString 1: '%s'\n", str1);
+    printf("\tString 2: '%s'\n", str2);
+    printf("\tResult: %d\n\n", mystrncmp(str1, str2, 5));
+
+    // Test Case #3: Same as first test case in this series, but string order is swapped
+    str1 = "hello";
+
+    printf("Case #3: Same as Case #1 in this series, but string order is swapped when passed to function\n");
+    printf("\tString 1: '%s'\n", str1);
+    printf("\tString 2: '%s'\n", str2);
+    printf("\tResult: %d\n\n", mystrncmp(str2, str1, 5));
+
+    // Test Case #4: One string shorter than the other and number of bytes longer than s1
+    str1 = "He";
+    str2 = "Hello to you?";
+
+    printf("Case #4: One string shorter than the other, and number of bytes longer than s1\n");
+    printf("\tString 1: '%s'\n", str1);
+    printf("\tString 2: '%s'\n", str2);
+    printf("\tResult: %d\n", mystrncmp(str1, str2, 5));
+    return 0;
 }
