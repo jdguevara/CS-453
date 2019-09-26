c75b5f08e38295189d73b2d78b473c5a0a273268
Jaime David Guevara Rojas <jaimeguevara@u.boisestate.edu>
2019-09-26 12:32:15 -0600

    Updates to p1

--- p1/fastlog/fastlog.c
+++ p1/fastlog/fastlog.c
@@ -9,28 +9,48 @@ struct entry {
     char message[MAX_MSG_LENGTH];
 };
 
-
+struct entry *bufferPtr;
+int counter = 0;
 
 void fastlog_init(void)
 {
 	// Set up a buffer array of entries of MAX_LOG_ENTRY size
-	struct entry buffer[MAX_LOG_ENTRY]; 
+	struct entry buffer[MAX_LOG_ENTRY];
+	bufferPtr = buffer;
 }
 
 
 void fastlog_write(LEVEL lvl, char *text)
 {
-    /*need to implement this.
-        
-    HINT: 
-    Don't forget that you should be copying stuff!!!
-    String assignments in C don't work the same as in 
-    java.
-    
-     */
+    // Check if we need to reset
+    check_buff_end();
+    time_t ltime;
+    ltime = time(NULL);
+
+    buffer[counter]->lvl = lvl;
+    buffer[counter]->message = strcpy(text);
+    buffer[counter]->pid = getpid();
+    buffer[counter]->time = ltime;
+    counter++;
 }
 
+void check_buff_end() {
+    if (counter == 500) {
+        counter = 0;
+    }
+}
+
+/**
+ * Print all valid logs to stderr using the following format:
+ * [pid]-[timestamp]-[LEVEL]-<message_text>"
+ *
+ * NOTE: Logs should be printed from oldest to newest
+ */
 void fastlog_dump(void) 
 {
-
+    int counter = 0;
+    while (bufferPtr) {
+        printf("[%ld]-[%s]-[%d]-<%s>", *(bufferPtr + counter)->pid, asctime(*(bufferPtr + counter)->time), *(bufferPtr + counter)->lvl, *(bufferPtr + counter)->message);
+        counter++;
+    }
 }
--- p1/fastlog/mytests.c
+++ p1/fastlog/mytests.c
@@ -4,6 +4,17 @@
 int main (void)
 
 {
-
+    fastlog_init();
+    fastlog_write(ERROR, "1");
+    fastlog_write(ERROR, "2");
+    fastlog_write(ERROR, "3");
+    fastlog_write(ERROR, "4");
+    fastlog_write(ERROR, "5");
+    fastlog_write(ERROR, "6");
+    fastlog_write(ERROR, "7");
+    fastlog_write(ERROR, "8");
+    fastlog_write(ERROR, "9");
+    fastlog_write(ERROR, "10");
+    fastlog_dump();
     return 0;
 }
