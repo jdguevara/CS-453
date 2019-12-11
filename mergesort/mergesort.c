dd4658990a39b4013ac4aeb00f33c8eee81ac909
Jaime Guevara <jdguevara93@gmail.com>
2019-12-10 23:53:11 -0700

    more changes

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -15,7 +15,7 @@ void serial_mergesort(int A[], int p, int r);
 void merge(int A[], int p, int q, int r);
 void insertion_sort(int A[], int p, int r);
 void parallel_mergesort(int A[], int p, int r, int numthreads);
-void *serial_mergesort(void *)
+void *mergesort(void *args);
 
 const int INSERTION_SORT_THRESHOLD = 100; //based on trial and error
 
