ad9d028e923f509e87a5f6bc2026daac949d3869
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2019-12-15 00:55:36 -0700

    Finished the last round of effectiveness

--- mergesort/merge.sh
+++ mergesort/merge.sh
@@ -3,9 +3,9 @@
 # This is to help me run the effectiveness tests so I don't have to sit here waiting for each one to finish
 
 x=1000000
-y=5
+y=1
 for ((i=1; i<101; i++))
 do
 	elements=$(($x * $i))
-	./mytests "$elements" "$y"
+	./mytests "$elements" "$y" "-f"
 done
