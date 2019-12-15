106ac6c76a00b9abf2360365b0b0671652eb9727
Jaime Guevara <jaimeguevara@u.boisestate.edu>
2019-12-14 23:34:11 -0700

    getting new stuff

---
+++ mergesort/merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is to help me run the effectiveness tests so I don't have to sit here waiting for each one to finish
+
+x=1000000
+y=5
+for ((i=1; i<101; i++))
+do
+	elements=$(($x * $i))
+	./mytests "$elements" "$y"
+done
